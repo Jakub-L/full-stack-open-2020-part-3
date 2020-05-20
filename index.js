4ec21765f2479d115e38b65d618c1afae958332d
Jakub <47220449+Jakub-L@users.noreply.github.com>
2020-05-20 16:34:12 +0100

    Solve exercise 3.8

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
-const express = require("express");
+const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-app.use(morgan('tiny'));
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body);
+});
+
 app.use(express.json());
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms - :body'
+  )
+);
 
 let persons = [
   {
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
     id: 1,
   },
   {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
     id: 2,
   },
   {
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
     id: 3,
   },
   {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
     id: 4,
   },
 ];
 
 app
-  .get("/api/persons", (req, res) => {
+  .get('/api/persons', (req, res) => {
     res.json(persons);
   })
-  .post("/api/persons", (req, res) => {
+  .post('/api/persons', (req, res) => {
     const { name, number } = req.body;
     if (!name || !number) {
       res
         .status(400)
         .json({ error: "Name and number can't be missing or blank" });
     } else if (persons.some((person) => person.name === name)) {
-      res.status(400).json({ error: "Person with this name already exists" });
+      res.status(400).json({ error: 'Person with this name already exists' });
     } else {
       const id = Math.floor(Math.random() * 1000);
       const newPerson = { name, number, id };
@@ -49,18 +57,18 @@ app
   });
 
 app
-  .get("/api/persons/:id", (req, res) => {
+  .get('/api/persons/:id', (req, res) => {
     const { id } = req.params;
     const person = persons.find((person) => person.id == id);
     res.json(person);
   })
-  .delete("/api/persons/:id", (req, res) => {
+  .delete('/api/persons/:id', (req, res) => {
     const { id } = req.params;
     persons = persons.filter((person) => person.id != id);
     res.status(204).end();
   });
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   res.send(
     `<p>Phonebook has information for ${
       persons.length
