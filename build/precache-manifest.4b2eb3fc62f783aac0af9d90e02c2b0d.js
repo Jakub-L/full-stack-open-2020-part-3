34eb3b54fe9022a1eb841972d7a1b32d52d70268
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-05-25 15:35:26 +0100

    Add build for frontend

---
+++ build/precache-manifest.4b2eb3fc62f783aac0af9d90e02c2b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9d43e540e96653362ee7c426934cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc0ea15c02165c4985e4",
+    "url": "/static/css/main.9a71dd4b.chunk.css"
+  },
+  {
+    "revision": "264ed0449746579c9fdc",
+    "url": "/static/js/2.25604a61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25604a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0ea15c02165c4985e4",
+    "url": "/static/js/main.cb4cf596.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
